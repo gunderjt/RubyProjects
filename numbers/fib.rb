5dbcdd9e65f67a6eeef25347bead0a523a64eb8c
Jeffrey G <gunderjt@gunderjt-VirtualBox.(none)>
2013-07-24 09:50:29 -0600

    True pi to N and fib sequence

---
+++ numbers/fib.rb
@@ -0,0 +1,14 @@
+def fib(n)
+  if n < 1
+    return 0
+  elsif n == 2
+    return 1
+  else
+    return fib(n-1) + fib(n-2)
+  end
+end
+
+puts "Enter N\n"
+n = STDIN.gets.chomp
+n = n.to_i
+puts fib(n)
